5b2be2487c7a750337a6d40825685c249844e0f4
Roman Hnatiuk <roman.hnatiuk02@gmail.com>
2023-12-09 18:09:16 +0200

    configure models

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,2 +1,7 @@
 class Contributor < ApplicationRecord
+  has_many :events
+
+  def total_score
+    events.sum(&:score)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,18 @@
 class Event < ApplicationRecord
   belongs_to :contributor
+
+  enum event_type: { pull_request: 12, comment: 1, review: 3 }
+
+  validates :event_type, presence: true
+  validates :contributor, presence: true
+
+  after_create :update_contributor_score
+
+  def update_contributor_score
+    contributor.update(total_score: contributor.total_score + score)
+  end
+
+  def score
+    Event.event_types[event_type]
+  end
 end
